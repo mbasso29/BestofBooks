30c3b9ff56c9442716f40da07b4fd0fc56c6d91d
Super-Derek <69864186+Super-Derek@users.noreply.github.com>
2023-02-15 18:04:45 -0500

    Added CreateUser, LoginModal, Admin View

---
+++ BestofBooks/BestofBooks/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace BestofBooks.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+        public bool AddsEnabled { get; set; }
+        public bool EditsEnabled { get; set; }
+        public bool DeletesEnabled { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsViewOnly { get; set; }
+    }
+
+}
