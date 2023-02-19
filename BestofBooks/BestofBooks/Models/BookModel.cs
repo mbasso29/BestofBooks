a182f4be6cc172fd3df5513a8970da8166744097
Super-Derek <69864186+Super-Derek@users.noreply.github.com>
2023-02-19 12:14:57 -0500

    Added new views for change report and available inventory report

---
+++ BestofBooks/BestofBooks/Models/BookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace BestofBooks.Models
+{
+	public class BookModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string AuthorFirst { get; set; }
+		public string AuthorLast { get; set; }
+		public string Genre { get; set; }
+		public bool InStock { get; set; }
+		public string ISBN { get; set; }
+		public string Location { get; set; }
+		public decimal Price { get; set; }
+	}
+}
+
