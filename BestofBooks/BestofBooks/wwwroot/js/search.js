1482635768ca8f504f5235170f8f041784659400
Super-Derek <69864186+Super-Derek@users.noreply.github.com>
2023-03-03 23:38:23 -0500

    Created issue where print function didn't work. Moved JS to view-specific files.
    Also removed print button from search page
    New issue where search page search button no longer clears search criteria
    Committing only green changes

---
+++ BestofBooks/BestofBooks/wwwroot/js/search.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/* Add event listener to Inventory Search buttons */
+document.getElementById("SearchBtn").addEventListener("click", function () {
+    clearInput("textFilter");
+    resetSelect("searchOptions");
+})
+
